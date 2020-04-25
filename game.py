8f9757ee05c9070a13141ad663255073e9ce16b1
AyusDas <ayus@users.noreply.github.com>
2020-04-25 17:56:49 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,47 @@
-"""
-Starting Template
 
-Once you have learned how to use classes, you can begin your program with this
-template.
-
-If Python and Arcade are installed, this example can be run from the command line with:
-python -m arcade.examples.starting_template
-"""
 import arcade
-import os
 import random
+
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
 RECTANGLE_COUNT = 25
-SCREEN_TITLE = "Starting Template"
+SCREEN_TITLE = "Destroy The Boxes"
 
 
-class MyGame(arcade.Window):
-    """
-    Main application class.
+class StartView(arcade.View):
 
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
+    def __init__(self):
+        super().__init__()
+        
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BLACK)
 
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+    def on_draw(self):
+        arcade.start_render()
+        msg = "PRESS ENTER TO START"
+        arcade.draw_text(msg, SCREEN_WIDTH/2, SCREEN_HEIGHT/2, arcade.color.ALABAMA_CRIMSON, 28, anchor_x= "center")
+
+    def on_key_press(self, key, _modifiers):
+
+        if key == arcade.key.ENTER :
+            gameview = MyGame()
+            gameview.setup()
+            self.window.show_view(gameview)    
+
+
+class MyGame(arcade.View):
+
+    def __init__(self):
+
+        super().__init__()
 
         arcade.set_background_color(arcade.color.BLACK)
-        self.set_mouse_visible(False)
+        
         self.dx = 3
         self.dy = 3
-        #  sprite lists
+        
+        #sprite lists
 
         #Player-------------------
         self.player_list = None
@@ -45,13 +53,9 @@ class MyGame(arcade.Window):
         self.ball_sprite = None
         #-------------------------
 
-        #Rectangles---------------
-        self.blue_rec_list = None
-        self.red_rec_list = None
-        self.lime_rec_list = None
-        self.yellow_rec_list = None
-        self.purple_rec_list = None
-        #--------------------------
+        self.box_rec_list = None
+        self.score = 0
+
 
     def setup(self):
         #  sprites and sprite lists 
@@ -60,127 +64,88 @@ class MyGame(arcade.Window):
         #                       Player
         #--------------------------------------------------------------
         self.player_list = arcade.SpriteList()
-        self.player_sprite = arcade.Sprite("bridgeB.png",0.5)
+        self.player_sprite = arcade.Sprite("resources/bridgeB.png",0.5)
         self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 25
+        self.player_sprite.center_y = 30
         self.player_list.append(self.player_sprite)
 
         #---------------------------------------------------------------
         #                        Ball
         #---------------------------------------------------------------
         self.ball_list = arcade.SpriteList()
-        self.ball_sprite = arcade.Sprite("meteor.png", 0.6)
+        self.ball_sprite = arcade.Sprite("resources/meteor.png", 0.6)
         self.ball_sprite.center_x = 100
         self.ball_sprite.center_y = 100
         self.ball_list.append(self.ball_sprite)
 
-        #----------------------------------------------------------------
-        #                        Rectangles
-        #----------------------------------------------------------------
         self.box_rec_list = arcade.SpriteList()
        
-
         for i in range(RECTANGLE_COUNT):
 
-            box_rec = arcade.Sprite("boxCrate_double.png",0.5)
+            box_rec = arcade.Sprite("resources/boxCrate_double.png",0.5)
 
             box_rec.center_x = random.randrange(5,SCREEN_WIDTH-5)
-            box_rec.center_y = random.randrange(200,SCREEN_HEIGHT-5)
+            box_rec.center_y = random.randrange(220,SCREEN_HEIGHT-5)
 
             self.box_rec_list.append(box_rec)
             
 
-
     def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
+        
         arcade.start_render()
-
+        
+        #draw the sprites----------------------
         self.player_list.draw()
         self.ball_list.draw()
-
         self.box_rec_list.draw()
-       
+        #--------------------------------------
         
+        # display the score
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.ALABAMA_CRIMSON, 16)
 
-        # Call draw() on all your sprite lists below
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
+        
         self.ball_sprite.center_x += self.dx
         self.ball_sprite.center_y += self.dy
 
         self.box_rec_list.update()
-        
-
 
         if self.ball_sprite.center_x < 5 or self.ball_sprite.center_x > SCREEN_WIDTH - 5 :
             self.dx *= -1
         if self.ball_sprite.center_y > SCREEN_HEIGHT - 5 :
             self.dy *= -1
-        if arcade.check_for_collision(self.player_sprite, self.ball_sprite):
+        if arcade.check_for_collision(self.player_sprite, self.ball_sprite) and self.player_sprite.center_y < self.ball_sprite.center_y:                      
             self.dy *= -1  
         
         hit_list_blue = arcade.check_for_collision_with_list(self.ball_sprite, self.box_rec_list)
         for box_rec in hit_list_blue:
-            if self.ball_sprite.center_y < box_rec.center_y - 1 or self.ball_sprite.center_y > box_rec.center_y + 1 :
+            if self.ball_sprite.center_y <= box_rec.center_y - 32 or self.ball_sprite.center_y >= box_rec.center_y + 32 :
                 self.dx *= 1
                 self.dy *= -1
             else:    
                 self.dx *= -1
                 self.dy *= 1
             box_rec.remove_from_sprite_lists()
+            self.score += 1
         
-       
-        
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+        if self.ball_sprite.center_y < 0 :
+            start = StartView()
+            self.window.show_view(start)
 
-        For a full list of keys, see:
-        http://arcade.academy/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
+
         self.player_sprite.center_x = x
-        #self.player_sprite.center_y = SCREEN_HEIGHT - 10
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
 
 
 def main():
     """ Main method """
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.set_mouse_visible(False)
+    start = StartView()
+    window.show_view(start)
     arcade.run()
 
 
